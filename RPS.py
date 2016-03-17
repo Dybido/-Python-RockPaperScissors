b103622acbf18caff99f56dde0fd64c1bb244cb0
Dybido <dybido93@gmail.com>
2016-03-18 02:47:22 +1100

    +Combined losing conditions +Created a system for keeping track of scores but doesn't work well +Allow user to choose to replay game

--- RPS.py
+++ RPS.py
@@ -1,40 +1,58 @@
 #!/usr/bin/python
 
-import os,sys, random
+import os, sys, random
+
+def condition(u, v):
+	user = 0
+	computer = 0
+
+	if u == v:
+		print "It's a tie!"
+	elif u == "rock" and v == "scissors":
+		print "You win! :)"
+		user += 1
+	elif u == "rock" and v == "paper": 
+		print "You lose! :("
+		computer += 1
+	elif u == "scissors" and v == "paper":
+		print "You win! :)"
+		user += 1
+	elif u == "scissors" and v == "rock": 
+		print "You lose! :("
+		computer += 1
+	elif u == "paper" and v == "rock":
+		print "You win! :)"
+		user += 1
+	else: #choice == "paper" and computer_choice == "scissors": 
+		print "You lose! :("
+		computer += 1
+	# Python can return multiple variables using tuples, dicts & lists to be returned 	
+	return (user, computer)
 
 print "Welcome to a game of Rock, Paper, Scissors!"
 
-choice = raw_input("To begin, please type rock, paper or scissors \n")
+while 1:
+	choice = raw_input("To begin, please type rock, paper or scissors \n")
 
-dice = random.randint(1,6)
+	dice = random.randint(1,6)
 
-if dice <= 2:
-	computer_choice = "rock"
-elif dice > 2 and dice <= 4:
-	computer_choice = "paper"
-else:
-	computer_choice = "scissors"	
+	if dice <= 2:
+		computer_choice = "rock"
+	elif dice > 2 and dice <= 4:
+		computer_choice = "paper"
+	else:
+		computer_choice = "scissors"	
 
-print "Your choice is", choice
-print "The computer's choice is", computer_choice
+	print "Your choice is", choice
+	print "The computer's choice is", computer_choice
 
-if choice == "rock" and computer_choice == "rock":
-	print "It's a tie!"
-elif choice == "rock" and computer_choice == "scissors":
-	print "You win! :)"
-elif choice == "rock" and computer_choice == "paper": 
-	print "You lose! :("
+	(p1,p2) = condition(choice, computer_choice)
 
-if choice == "scissors" and computer_choice == "scissors":
-	print "It's a tie!"
-elif choice == "scissors" and computer_choice == "paper":
-	print "You win! :)"
-else: 
-	print "You lose! :("
+	print "The score is now You/Computer:", p1, "/", p2
 
-if choice == "paper" and computer_choice == "paper":
-	print "It's a tie!"
-elif choice == "paper" and computer_choice == "rock":
-	print "You win! :)"
-else: 
-	print "You lose! :("
+	cont = raw_input("Would you like to continue playing? (yes/no) \n")
+	if cont == "no":
+		break
+	else: 
+		pass
+	
